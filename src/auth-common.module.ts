805b4be5f7ade49daabe52034024f810e1958592
Desh Raj <deshrajmnitj@gmail.com>
2022-08-14 00:22:40 +0530

    initial commit

--- src/auth-common.module.ts
+++ src/auth-common.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RolesGuard } from './guard/roles.guard';
-import { Public } from './guard/public.guard';
 import { JwtAuthGuard } from './guard/jwt-auth.guard';
-import { Roles } from './decorator/roles.decorator';
-import { CurrentUser } from './decorator/current-user.decorator';
 
 @Module({
-  exports: [RolesGuard, Public, JwtAuthGuard, Roles, CurrentUser]
+  providers: [RolesGuard, JwtAuthGuard],
+  exports: [RolesGuard, JwtAuthGuard]
 })
-export class AuthCommon {}
+export class AuthCommonModule { }
+export { RolesGuard, JwtAuthGuard };
